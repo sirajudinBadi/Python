bf91b8b8cfdbf137f01516640a23cbb88d28881d
Sirajudin Badi <sirubadi79@gmail.com>
2025-07-21 15:16:54 +0530

    Completed the collections

---
+++ 01_basic/second.py
@@ -0,0 +1,15 @@
+from collections import Counter
+from itertools import combinations
+
+# l = [1, 2, 3, 4, 2, 3, 1, 5, 6]
+# l = ["a", 'b', 'a', 'c', 'd', 'e', 'd']
+# d = dict(Counter(l))
+# print(d)
+
+# combs = []
+# l = [1, 2, 3]
+# for i in range(1, len(l)+1):
+#     for combo in combinations(l, i):
+#         combs.append(list(combo))
+# print(combs)
+
