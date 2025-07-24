648f091d899d5afcecd640b72a06cf4c73e35e79
Sirajudin Badi <sirubadi79@gmail.com>
2025-07-24 15:54:38 +0530

    Modified the format

--- Tasks/Basic/11_filter_lambda.py
+++ Tasks/Basic/11_filter_lambda.py
@@ -1,5 +1,3 @@
-from itertools import combinations, permutations
-
 
 test_list = [3456, 23, 128, 235, 982]
 digit_list = [2, 3, 5, 4]
@@ -8,16 +6,3 @@ allowed_digits = [str(i) for i in digit_list]
 filtered_list = list(filter(lambda x : all(digit in allowed_digits for digit in str(x)), test_list))
 print(filtered_list)
 
-# combs = []
-# for r in range(1,len(digit_list)+1):
-#     combs.extend(combinations(digit_list, r))
-# print(combs)
-#
-# str_test = []
-# for num in test_list:
-#     num_split = str(num)
-#     str_test.append(num_split)
-# print(str_test)
-# for ls in combs:
-#     listed = list(ls)
-#     print(listed, end=" ")
